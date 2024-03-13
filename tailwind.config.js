75b9ba1d8f87080b7a2ff0419b022597ac8408a8
enrihg <99612570+enrihg@users.noreply.github.com>
2024-03-13 10:58:07 -0300

    header added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'body-regular': ['Space Mono Regular', 'sans-serif'],
+        'body-bold': ['Space Mono Bold', 'sans-serif']
+      },
+      colors: {
+        'blue1': '#0079ff',
+        'blue2': '#4b6a9b',
+        'blue3': '#697c9a',
+        'blue4': '#2b3442',
+        'blue5': '#1e2a47',
+        'blue6': '#141d2f',
+        'white1': '#fefefe',
+        'white2': '#f6f8ff'
+      }
+    },
   },
   plugins: [],
 }
